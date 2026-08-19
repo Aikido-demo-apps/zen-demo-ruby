54b158e9c58352e67024217cd926dcc7110c2948
Mark Smith <mark@aikido.dev>
2026-08-19 15:37:49 +0200

    Set custom client IP header from environment variable

--- config/initializers/zen.rb
+++ config/initializers/zen.rb
@@ -1,6 +1,5 @@
 if Rails.application.config.respond_to?(:zen)
   zen = Rails.application.config.zen
 
-  zen.client_ip_header = "HTTP_FLY_CLIENT_IP"
   zen.realtime_settings_updates_enabled = true
 end
